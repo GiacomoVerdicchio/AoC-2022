0510720da4f91d0e01fb59001176ac9bca2a745b
Giacomo <giacomo.verdicchio@mail.polimi.it>
2024-09-13 15:38:20 +0200

    Resolve problem in running AoC

--- Aoc-2022--new/src/main/java/AoC1.java
+++ Aoc-2022--new/src/main/java/AoC1.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class Main {
+public class AoC1 {
     public static void main(String[] args) throws IOException {
         String path = "src/main/resources/input.txt";
 
